9bd5a63ed5436ef759244a0293b081ee519f0574
Raj <rajprofessional1991@gmail.com>
2021-05-29 06:12:27 +0530

    up

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,8 +29,12 @@ module "vpc" {
   azs             = local.azone
   private_subnets = local.cidr_blk_pvt
   public_subnets  = local.cidr_blk_pub
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
   public_subnet_tags = {
     "kubernetes.io/cluster/Practise" = "shared"
+    "kubernetes.io/role/elb" = 1
   }
   enable_nat_gateway = true
   enable_vpn_gateway = true
